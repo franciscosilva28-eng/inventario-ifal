81a43b1b94a926a2659e5c20684893699aaabf8d
franciscosilva28-eng <francisco.silva28@gmail.com>
2026-09-21 11:23:30 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "inventario-cache-v3";
+
+const ARQUIVOS = [
+    "inventario.html",
+    "manifest.json",
+    "jszip.min.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(ARQUIVOS);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(chaves => {
+            return Promise.all(
+                chaves
+                    .filter(chave => chave !== CACHE_NAME)
+                    .map(chave => caches.delete(chave))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(resposta => {
+                return resposta || fetch(event.request);
+            })
+    );
+});
